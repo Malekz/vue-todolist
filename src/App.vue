6577dc3efd289a8621e5529fdcb235fe796dfb94
malekz <malek.zarzour@gmail.com>
2019-12-10 01:39:52 +0100

    finshed with router

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,34 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
-
 export default {
   name: "app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
-    }
+    Header
   }
 };
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+
+#nav {
+  padding: 30px;
 }
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
+
+#nav a {
+  font-weight: bold;
   color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
-}
-.btn:hover {
-  background: #666;
 }
 </style>
-
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a Todolist app v1.0.0. it is part of the Vue crash course on youtube.</p>
+  </div>
+</template>
